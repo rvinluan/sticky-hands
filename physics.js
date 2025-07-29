2b65e8d6521f0b6c5f9729881ab38b63d515c0fd
Robert Vinluan <robertvinluan@Roberts-MacBook-Pro.local>
2025-07-28 23:16:55 -0400

    small fixes

--- physics.js
+++ physics.js
@@ -79,7 +79,8 @@ function initializePhysics() {
         // Apply force to chain depending on which side of the screen it is on
         playerPhysicsHands.forEach(hand => {
             if (hand.ball.render.sprite.texture.indexOf('thumb') !== -1) {
-                Matter.Body.setAngle(hand.ball, Math.PI/4);
+                console.log("setting angle");
+                Matter.Body.setAngle(hand.ball, Math.PI*0.5);
             }
             Composite.allBodies(hand.composite).forEach(body => {
                 if(hand.composite.bodies[0].position.y <= window.innerHeight / 2) {
